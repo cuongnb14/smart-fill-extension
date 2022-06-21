4e691420205ca56eed8b134a540fc4b4cf74a018
Cuong Nguyen <cuongnb14@gmail.com>
2022-06-21 17:16:05 +0700

    optimize inject content script

--- background.js
+++ background.js
@@ -1,12 +1,37 @@
-function fill(info, tab) {
-  chrome.scripting.executeScript({target: {tabId: tab.id}, files: ["libs/jquery-3.6.0.min.js", "libs/faker-3.1.0.js", "auto_fill.js"] }, () => {
+var injected = false;
 
-  })
+function injectedFunction() {
+  auto_fill()
+}
+
+function fill(tab) {
+  if(injected) {
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tab.id },
+        function: injectedFunction
+      }
+    )
+
+  } else {
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tab.id },
+        files: [
+          "libs/jquery-3.6.0.min.js",
+          "libs/faker-3.1.0.js", 
+          "auto_fill.js"
+        ]
+      }
+    )
+    injected = true;
+  }
+  
 }
 
 
 chrome.action.onClicked.addListener(
   function (tab) {
-    fill(null, tab)
+    fill(tab)
   }
 );
